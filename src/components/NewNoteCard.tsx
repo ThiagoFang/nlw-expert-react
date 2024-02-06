c91c514092cc29bf0ce3c31d98b71424bad8d50f
Thiago Fang <thiagofang@gmail.com>
2024-02-06 19:08:58 -0300

    metade da aula 2

--- src/components/NewNoteCard.tsx
+++ src/components/NewNoteCard.tsx
@@ -1,12 +1,37 @@
+import { X } from "lucide-react";
+
+import * as Dialog from "@radix-ui/react-dialog";
+
 export function NewNoteCard() {
   return (
-    <div className="relative p-5 space-y-3 overflow-hidden rounded-md bg-slate-700">
-      <span className="text-sm font-medium text-slate-200">Adicionar Nota</span>
-      <p className="text-sm leading-6 text-slate-400">
-        Grave uma nota em áudio que será convertida para texto automaticamente.
-      </p>
+    <Dialog.Root>
+      <Dialog.Trigger className="relative flex flex-col gap-4 p-5 overflow-hidden text-left rounded-md outline-none focus-visible:ring-2 focus-visible:ring-lime-400 hover:ring-2 ring-slate-600 bg-slate-700">
+        <span className="text-sm font-medium text-slate-200">Adicionar Nota</span>
+        <p className="text-sm leading-6 text-slate-400">
+          Grave uma nota em áudio que será convertida para texto automaticamente.
+        </p>
 
-      <div className="absolute bottom-0 left-0 right-0 pointer-events-none bg-gradient-to-t from-black/60 to-black/0 h-1/2" />
-    </div>
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-black/60" />
+        <Dialog.Content className="fixed overflow-hidden -translate-x-1/2 h-[60vh] -translate-y-1/2 left-1/2 top-1/2 max-w-[640px] w-full bg-slate-700 rounded-md flex flex-col outline-none">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
+          </Dialog.Close>
+
+          <div className="flex flex-col flex-1 gap-3 p-5">
+            <span className="text-sm font-medium text-slate-3-300">Adicionar Nota</span>
+            <p className="text-sm leading-6 text-slate-400">
+              Comece <button type="button" className="text-lime-400 hover:underline">gravando uma nota</button> em áudio ou se preferir <button type="button" className="text-lime-400 hover:underline">utilize apenas texto</button>.
+            </p>
+          </div>
+
+          <button type="button" className="w-full py-4 text-sm text-center outline-none group bg-lime-400 hover:bg-lime-500 text-lime-950" >
+            Salvar nota
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -1,3 +1,9 @@
+import { formatDistanceToNow } from "date-fns"
+import { ptBR } from "date-fns/locale"
+
+import * as Dialog from "@radix-ui/react-dialog"
+import { X } from "lucide-react"
+
 interface NodeCardProps {
   note: {
     date: Date,
@@ -6,16 +12,42 @@ interface NodeCardProps {
 }
 
 export function NoteCard({ note }: NodeCardProps) {
-  const { content, date } = note
+  const date = formatDistanceToNow(note.date, {
+    locale: ptBR,
+    addSuffix: true
+  })
+
 
   return (
-    <button className="relative flex flex-col gap-3 p-5 overflow-hidden text-left rounded-md outline-none focus-visible:ring-2 focus-visible:ring-lime-400 hover:ring-2 ring-slate-600 bg-slate-800">
-      <span className="text-sm font-medium text-slate-3-300">{date.toISOString()}</span>
-      <p className="text-sm leading-6 text-slate-400">
-        {content}
-      </p>
+    <Dialog.Root>
+      <Dialog.Trigger className="relative flex flex-col gap-3 p-5 overflow-hidden text-left rounded-md outline-none focus-visible:ring-2 focus-visible:ring-lime-400 hover:ring-2 ring-slate-600 bg-slate-800">
+        <span className="text-sm font-medium text-slate-3-300">{date}</span>
+        <p className="text-sm leading-6 text-slate-400">
+          {note.content}
+        </p>
 
-      <div className="absolute bottom-0 left-0 right-0 pointer-events-none bg-gradient-to-t from-black/60 to-black/0 h-1/2" />
-    </button>
+        <div className="absolute bottom-0 left-0 right-0 pointer-events-none bg-gradient-to-t from-black/60 to-black/0 h-1/2" />
+      </Dialog.Trigger>
+
+      <Dialog.Portal>
+        <Dialog.Overlay className="fixed inset-0 bg-black/60" />
+        <Dialog.Content className="fixed overflow-hidden -translate-x-1/2 h-[60vh] -translate-y-1/2 left-1/2 top-1/2 max-w-[640px] w-full bg-slate-700 rounded-md flex flex-col outline-none">
+          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100">
+            <X className="size-5" />
+          </Dialog.Close>
+
+          <div className="flex flex-col flex-1 gap-3 p-5">
+            <span className="text-sm font-medium text-slate-3-300">{date}</span>
+            <p className="text-sm leading-6 text-slate-400">
+              {note.content}
+            </p>
+          </div>
+
+          <button type="button" className="w-full py-4 text-sm text-center outline-none group bg-slate-800 text-slate-300" >
+            Deseja <span className="text-red-400 group-hover:underline">apagar essa nota?</span>
+          </button>
+        </Dialog.Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   )
 }
